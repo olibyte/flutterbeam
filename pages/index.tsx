a919eb2adbde93a85323347afb2f2515ca49061a
Oliver <ocben1@gmail.com>
2023-06-12 12:16:55 +1000

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,25 +27,15 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
         <WhiteBackgroundContainer>
           <Hero />
           <Partners />
-          <BasicSection imageUrl="/demo-illustration-1.svg" title="Lorem ipsum dolor sit amet consectetur." overTitle="sit amet gogo">
+          <BasicSection imageUrl="/demo-illustration-1.svg" title="Our Story" overTitle="The Past">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore
-              voluptate quo deleniti animi laboriosam.{' '}
-              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Soluta repellendus quia quos obcaecati nihil. Laudantium non accusantium, voluptate eum nesciunt
-              at suscipit quis est soluta?
+            Founded in Melbourne in 2018, Flutterbeam embarked on a mission to transform the e-learning landscape by delivering innovative solutions to universities and the Victorian government. {' '}
+              <Link href="/help-center">Possimus ullam velit rem itaque consectetur, in distinctio?</Link> Recognizing the evolving needs of educational institutions in the digital age, we set out to create a platform that would revolutionize the way knowledge is imparted and acquired. Starting with a small team of passionate educators and tech enthusiasts, we began collaborating closely with universities and government bodies, understanding their unique requirements and challenges.
             </p>
           </BasicSection>
-          <BasicSection imageUrl="/demo-illustration-2.svg" title="Lorem ipsum dolor sit." overTitle="lorem ipsum" reversed>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, quidem error incidunt a doloremque voluptatem porro inventore{' '}
-              <strong>voluptate quo deleniti animi laboriosam</strong>. Possimus ullam velit rem itaque consectetur, in distinctio?
+          <BasicSection imageUrl="/demo-illustration-2.svg" title="The digital era" overTitle="The Present" reversed>
+            <p>Over the years, Flutterbeam has grown exponentially, expanding our team of talented professionals and continuously refining our platform. Our focus on harnessing the power of technology, combined with our deep understanding of the educational landscape, has allowed us to enhance access to quality education, empower learners, and equip institutions with the tools they need to thrive in the digital era.
             </p>
-            <ul>
-              <li>Professional point 1</li>
-              <li>Professional remark 2</li>
-              <li>Professional feature 3</li>
-            </ul>
           </BasicSection>
         </WhiteBackgroundContainer>
         <DarkerBackgroundContainer>
